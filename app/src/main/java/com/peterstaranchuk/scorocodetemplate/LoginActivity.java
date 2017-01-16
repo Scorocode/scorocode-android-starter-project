6f829c8710b111ece3e04db2b2710b47b5c16523
peter <petr.staranchuk@gmail.com>
2017-01-16 13:51:19 +0300

    initial state

---
+++ app/src/main/java/com/peterstaranchuk/scorocodetemplate/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.peterstaranchuk.scorocodetemplate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.etLogin) EditText etLogin;
+    @BindView(R.id.etPassword) EditText etPassword;
+    @BindView(R.id.btnLogin) Button btnLogin;
+    @BindView(R.id.btnRegister) Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void onLoginButtonClicked(View loginButton) {
+        String login = etLogin.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if(!login.isEmpty() && !password.isEmpty()) {
+
+        }
+    }
+}
