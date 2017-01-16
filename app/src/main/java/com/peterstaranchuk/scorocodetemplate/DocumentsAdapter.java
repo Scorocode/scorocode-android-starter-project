d4620977d06355d80cfa991492fb8bff59c5783b
peter <petr.staranchuk@gmail.com>
2017-01-16 14:58:10 +0300

    add document list screen

---
+++ app/src/main/java/com/peterstaranchuk/scorocodetemplate/DocumentsAdapter.java
@@ -0,0 +1,80 @@
+package com.peterstaranchuk.scorocodetemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ru.profit_group.scorocode_sdk.scorocode_objects.Document;
+import ru.profit_group.scorocode_sdk.scorocode_objects.DocumentInfo;
+
+/**
+ * Created by Peter Staranchuk.
+ */
+
+public class DocumentsAdapter extends BaseAdapter {
+
+    private List<DocumentInfo> documents;
+    private LayoutInflater inflater;
+    private int layoutRes;
+
+    public DocumentsAdapter(Context context, List<DocumentInfo> documents, int layoutRes) {
+        this.inflater = LayoutInflater.from(context);
+        this.documents = (documents == null? new ArrayList<DocumentInfo>() : documents);
+        this.layoutRes = layoutRes;
+    }
+
+    @Override
+    public int getCount() {
+        return documents.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if(position < documents.size()) {
+            return documents.get(position);
+        } else {
+            return new Document("");
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        ViewHolder holder;
+        if (view != null) {
+            holder = (ViewHolder) view.getTag();
+        } else {
+            view = inflater.inflate(layoutRes, parent, false);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        }
+
+        DocumentInfo document = (DocumentInfo) getItem(position);
+
+        holder.tvDocumentId.setText(document.get("_id").toString());
+        holder.tvDocumentName.setText(document.get("documentName").toString());
+
+        return view;
+    }
+
+    static class ViewHolder {
+        @BindView(R.id.tvDocumentId) TextView tvDocumentId;
+        @BindView(R.id.tvDocumentName) TextView tvDocumentName;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
--- app/src/main/java/com/peterstaranchuk/scorocodetemplate/ListActivity.java
+++ app/src/main/java/com/peterstaranchuk/scorocodetemplate/ListActivity.java
@@ -4,18 +4,72 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import ru.profit_group.scorocode_sdk.Callbacks.CallbackLoginUser;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ru.profit_group.scorocode_sdk.Callbacks.CallbackFindDocument;
+import ru.profit_group.scorocode_sdk.scorocode_objects.DocumentInfo;
+import ru.profit_group.scorocode_sdk.scorocode_objects.Query;
 
 public class ListActivity extends AppCompatActivity {
+
+    @BindView(R.id.lvDocuments) ListView lvDocuments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        ButterKnife.bind(this);
+        refreshList();
+    }
+
+    private void refreshList() {
+        new Query(getString(R.string.collectionName))
+                .findDocuments(new CallbackFindDocument() {
+                    @Override
+                    public void onDocumentFound(List<DocumentInfo> documentInfos) {
+                        DocumentsAdapter adapter = new DocumentsAdapter(ListActivity.this, documentInfos, R.layout.item_document);
+                        lvDocuments.setAdapter(adapter);
+                    }
+
+                    @Override
+                    public void onDocumentNotFound(String errorCode, String errorMessage) {
+                        Toast.makeText(ListActivity.this, R.string.errorDuringDocumentLoading, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     public static void display(Context context) {
         context.startActivity(new Intent(context, ListActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addDocument:
+                AddDocumentActivity.display(this);
+                return true;
+
+            case R.id.refreshList:
+                refreshList();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
