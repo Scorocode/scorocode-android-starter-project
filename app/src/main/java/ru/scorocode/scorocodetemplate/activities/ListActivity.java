996494c9fcbb8ea31325a31959f97fdf72286915
peter <petr.staranchuk@gmail.com>
2017-02-07 14:48:22 +0300

    add swipe\delete behaviour in ListView

--- app/src/main/java/ru/scorocode/scorocodetemplate/activities/ListActivity.java
+++ app/src/main/java/ru/scorocode/scorocodetemplate/activities/ListActivity.java
@@ -1,19 +1,15 @@
 package ru.scorocode.scorocodetemplate.activities;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.util.List;
@@ -21,8 +17,6 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import ru.profit_group.scorocode_sdk.Callbacks.CallbackFindDocument;
-import ru.profit_group.scorocode_sdk.Callbacks.CallbackRemoveDocument;
-import ru.profit_group.scorocode_sdk.Responses.data.ResponseRemove;
 import ru.profit_group.scorocode_sdk.scorocode_objects.DocumentInfo;
 import ru.profit_group.scorocode_sdk.scorocode_objects.Query;
 import ru.scorocode.scorocodetemplate.R;
@@ -45,7 +39,7 @@ public class ListActivity extends AppCompatActivity {
         refreshList();
     }
 
-    private void refreshList() {
+    public void refreshList() {
         //To get all documents from collection you should:
         //1.Create new object of Query class
         Query query = new Query(getString(R.string.collectionName));
@@ -58,11 +52,12 @@ public class ListActivity extends AppCompatActivity {
                 //As a result you have list of DocumentInfo objects
                 //All information about documents stored in this class
                 DocumentsAdapter adapter = new DocumentsAdapter(ListActivity.this, documentInfos, R.layout.item_document);
+
                 lvDocuments.setAdapter(adapter);
                 lvDocuments.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        showChooseActionDialog(documentInfos, position);
+                        DocumentActivity.showDocument(ListActivity.this, documentInfos.get(position));
                     }
                 });
             }
@@ -72,76 +67,6 @@ public class ListActivity extends AppCompatActivity {
                 //You can handle case if no documents were found.
                 //You can also see error code and message if searching process was failed
                 Toast.makeText(ListActivity.this, R.string.errorDuringDocumentLoading, Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private void showChooseActionDialog(final List<DocumentInfo> documentInfos, final int position) {
-        final View dialogView = LayoutInflater.from(ListActivity.this).inflate(R.layout.item_action_view, null);
-
-        new AlertDialog.Builder(ListActivity.this)
-                .setTitle(R.string.choose_action)
-                .setView(dialogView)
-                .setPositiveButton(R.string.continue_action, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                performActionWithDocument(dialogView, documentInfos, position);
-                            }
-                        }
-                )
-                .setNegativeButton(R.string.close_action, null)
-                .setCancelable(false)
-                .show();
-    }
-
-    private void performActionWithDocument(View dialogView, List<DocumentInfo> documentInfos, int position) {
-        //All information about document's fields and id's stored in
-        //document info class
-
-        final RadioGroup rgChooseItems = ButterKnife.findById(dialogView, R.id.rgChooseItems);
-        DocumentInfo selectedDocument = documentInfos.get(position);
-
-        switch (rgChooseItems.getCheckedRadioButtonId()) {
-            case R.id.rbOpen:
-                DocumentActivity.showDocument(ListActivity.this, selectedDocument);
-                break;
-
-            case R.id.rbEdit:
-                DocumentActivity.editDocument(ListActivity.this, selectedDocument);
-                break;
-
-            case R.id.rbRemove:
-                removeSelectedDocument(selectedDocument);
-                break;
-
-            default:
-                Toast.makeText(ListActivity.this, R.string.no_item_selected, Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void removeSelectedDocument(DocumentInfo clickedDocument) {
-        //To remove document from server (from collection) you should:
-        //1. create new Query class. You should also specify collection name in constructor
-        Query query = new Query(getString(R.string.collectionName));
-
-        //2. find document in collection using one of Query methods
-        //(in this case we searching for document with particular id)
-        query.equalTo("_id", clickedDocument.getId());
-
-        //3. Use removeDocument() method of Query class
-        query.removeDocument(new CallbackRemoveDocument() {
-            @Override
-            public void onRemoveSucceed(ResponseRemove responseRemove) {
-                //after removable process you can perform some actions
-                Toast.makeText(ListActivity.this, R.string.decument_removed, Toast.LENGTH_SHORT).show();
-                refreshList();
-            }
-
-            @Override
-            public void onRemoveFailed(String errorCode, String errorMessage) {
-                //if remove process failed you can handle this situation.
-                //you can also see code and message of error
-                Toast.makeText(ListActivity.this, R.string.error_during_doc_removal, Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/ru/scorocode/scorocodetemplate/adapters/DocumentsAdapter.java
+++ app/src/main/java/ru/scorocode/scorocodetemplate/adapters/DocumentsAdapter.java
@@ -5,16 +5,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.daimajia.swipe.SwipeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import ru.profit_group.scorocode_sdk.Callbacks.CallbackRemoveDocument;
+import ru.profit_group.scorocode_sdk.Responses.data.ResponseRemove;
 import ru.profit_group.scorocode_sdk.scorocode_objects.Document;
 import ru.profit_group.scorocode_sdk.scorocode_objects.DocumentInfo;
+import ru.profit_group.scorocode_sdk.scorocode_objects.Query;
 import ru.scorocode.scorocodetemplate.R;
+import ru.scorocode.scorocodetemplate.activities.DocumentActivity;
+import ru.scorocode.scorocodetemplate.activities.ListActivity;
 
 /**
  * Created by Peter Staranchuk.
@@ -22,12 +31,14 @@ import ru.scorocode.scorocodetemplate.R;
 
 public class DocumentsAdapter extends BaseAdapter {
 
+    private Context context;
     private List<DocumentInfo> documents;
     private LayoutInflater inflater;
     private int layoutRes;
 
     public DocumentsAdapter(Context context, List<DocumentInfo> documents, int layoutRes) {
         this.inflater = LayoutInflater.from(context);
+        this.context = context;
         this.documents = (documents == null? new ArrayList<DocumentInfo>() : documents);
         this.layoutRes = layoutRes;
     }
@@ -67,17 +78,68 @@ public class DocumentsAdapter extends BaseAdapter {
         //you can get id of document using getId() method
         holder.tvDocumentId.setText(document.getId());
         //you can get any field content from document by fields't name using get() method
-        holder.tvDocumentName.setText(document.get("documentName").toString());
+        holder.tvDocumentName.setText(document.get(context.getString(R.string.documentNameField)).toString());
 
+        holder.ivEdit.setOnClickListener(onEditListener(document));
+        holder.ivDelete.setOnClickListener(onDeleteListener(document));
         return view;
     }
 
     static class ViewHolder {
         @BindView(R.id.tvDocumentId) TextView tvDocumentId;
         @BindView(R.id.tvDocumentName) TextView tvDocumentName;
+        @BindView(R.id.ivEdit) ImageView ivEdit;
+        @BindView(R.id.ivDelete) ImageView ivDelete;
+        @BindView(R.id.swipeLayout) SwipeLayout swipeLayout;
 
         public ViewHolder(View view) {
             ButterKnife.bind(this, view);
+            swipeLayout.setShowMode(SwipeLayout.ShowMode.PullOut);
         }
+    }
+
+    private View.OnClickListener onEditListener(final DocumentInfo document) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DocumentActivity.editDocument(context, document);
+            }
+        };
+    }
+
+    private View.OnClickListener onDeleteListener(final DocumentInfo document) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeSelectedDocument(document);
+            }
+        };
+    }
+
+    private void removeSelectedDocument(DocumentInfo clickedDocument) {
+        //To remove document from server (from collection) you should:
+        //1. create new Query class. You should also specify collection name in constructor
+        Query query = new Query(context.getString(R.string.collectionName));
+
+        //2. find document in collection using one of Query methods
+        //(in this case we searching for document with particular id)
+        query.equalTo("_id", clickedDocument.getId());
+
+        //3. Use removeDocument() method of Query class
+        query.removeDocument(new CallbackRemoveDocument() {
+            @Override
+            public void onRemoveSucceed(ResponseRemove responseRemove) {
+                //after removable process you can perform some actions
+                Toast.makeText(context, R.string.decument_removed, Toast.LENGTH_SHORT).show();
+                ((ListActivity) context).refreshList();
+            }
+
+            @Override
+            public void onRemoveFailed(String errorCode, String errorMessage) {
+                //if remove process failed you can handle this situation.
+                //you can also see code and message of error
+                Toast.makeText(context, R.string.error_during_doc_removal, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
